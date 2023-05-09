bc2565eead88d3616a26146708eb944eba529704
Mathew Taylor <mathew.taylor@davies-group.com>
2023-05-09 11:19:48 +0100

    Cypress - Login Via UI

---
+++ ReactApplication/cypress/e2e/Login.cy.ts
@@ -0,0 +1,18 @@
+describe('Login', function () {
+  beforeEach(function () {
+    cy.visit(Cypress.env('app_url'))
+
+    cy.loginToAuth0(
+      Cypress.env('auth0_username'),
+      Cypress.env('auth0_password')
+    )
+  })
+
+  it('can log into application', function () {
+    cy.contains('Vite + React').should('be.visible')
+  })
+
+  it('can log into application', function () {
+    cy.contains('Vite + React').should('be.visible')
+  })
+})
--- ReactApplication/src/App.tsx
+++ ReactApplication/src/App.tsx
@@ -47,7 +47,7 @@ export function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React 123</h1>
+      <h1>Vite + React</h1>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
