af7998b74b6310c949f60139b148e969838d527a
Mathew Taylor <mathew.taylor@davies-group.com>
2023-05-10 09:03:21 +0100

    Vitest added

---
+++ ReactApplication/src/tests/Accordion.test.tsx
@@ -0,0 +1,12 @@
+import {describe, expect, test} from 'vitest';
+import {render, screen} from '@testing-library/react';
+import Accordion from './Accordion';
+
+describe("Accordion test", () => {
+    test("should show title all the time", () => {
+        
+        render(<Accordion title='Testing'><h4>Content</h4></Accordion>);
+
+        expect(screen.getByText(/Testing/i)).toBeDefined()
+    })
+})
