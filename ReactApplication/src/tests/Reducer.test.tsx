2c45fc358d8cf678e07172e1270cf3bf69db7f07
Mathew Taylor <mathew.taylor@davies-group.com>
2023-05-11 12:38:36 +0100

    Reducer Tests

--- ReactApplication/src/tests/Reducer.test.tsx
+++ ReactApplication/src/tests/Reducer.test.tsx
@@ -1,24 +1,45 @@
 import { describe, expect, test } from 'vitest';
-// import { store } from '../state/store';
-import { initialMockState } from '../utils/testUtil';
+import { store } from '../state/store';
+//import { initialMockState } from '../utils/testUtil';
+import { decrementNotifications, incrementNotifications } from '../state/slices/applicationSlice';
 
 describe('With React Testing Library', () => {
-  test('Drivers loaded into state', async () => {
-    const applicationState = initialMockState.application;
+  // test('Application Notifications loaded into state', async () => {
+  //   const applicationState = initialMockState.application;
+
+  //   const initialNotificationsCount = applicationState.notifications;
+
+  //   expect(initialNotificationsCount).toEqual(undefined);
+  // });
+
+  test('Application Notifications Incremented', async () => {
+    let applicationState = store.getState().application;
 
     const initialNotificationsCount = applicationState.notifications;
 
-    expect(initialNotificationsCount).toEqual(undefined);
+    await store.dispatch(incrementNotifications());
+
+    applicationState = store.getState().application;
+    expect(applicationState.notifications).toEqual(1);
+
+    await store.dispatch(incrementNotifications());
+
+    applicationState = store.getState().application;
+    expect(applicationState.notifications).toBeGreaterThan(initialNotificationsCount);
+    expect(applicationState.notifications).toEqual(2);
   });
 
-//   test('Loads job statuses into state', async () => {
-//     let applicationState = store.getState().application;
+  test('Application Notifications Decremented', async () => {
+    let applicationState = store.getState().application;
 
-//     const initialNotificationsCount = applicationState.notifications;
+    await store.dispatch(decrementNotifications());
 
-//     await applicationState.dispatch(getJobStatuses() as any);
+    applicationState = store.getState().application;
+    expect(applicationState.notifications).toEqual(-1);
 
-//     applicationState = store.getState().application;
-//     expect(applicationState.notifications).toBeGreaterThan(initialNotificationsCount);
-//   });
+    await store.dispatch(decrementNotifications());
+
+    applicationState = store.getState().application;
+    expect(applicationState.notifications).toEqual(-2);
+  });
 });
